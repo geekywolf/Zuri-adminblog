d0aa17c7dd2285c660f8e6787e511cda9633863b
Heretic <t4riumph@gmail.com>
2022-06-21 16:24:32 +0100

    created python model in app 'Blog'

--- I4G034216BR3/Blog/models.py
+++ I4G034216BR3/Blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+class Posts(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateField()
+    published_date = models.DateField(auto_now_add=True)
